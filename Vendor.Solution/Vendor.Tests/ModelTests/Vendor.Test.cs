a18c907a863024f2b1ca762333b6baa69c9c0f74
Amanda Otero <mandystar018@gmail.com>
2020-12-18 09:49:34 -0800

    Add more to my strucute project and start to test my constructor

--- Vendor.Solution/Vendor.Tests/ModelTests/Vendor.Test.cs
+++ Vendor.Solution/Vendor.Tests/ModelTests/Vendor.Test.cs
@@ -1,1 +1,8 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace Vendors.Test
+{
+  
+}
---
+++ Vendor.Solution/Vendor/Models/Vendor.cs
@@ -0,0 +1,6 @@
+using System.Collections.Generic;
+
+namespace Vendors.Models
+{
+  
+}
