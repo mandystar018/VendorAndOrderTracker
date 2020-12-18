219f407b481fa3fd47b82be5671203cd785c72b7
Amanda Otero <mandystar018@gmail.com>
2020-12-18 10:04:29 -0800

    Add Constructor of Order

---
+++ Vendor.Solution/Vendor.Tests/ModelTests/OrderTest.cs
@@ -0,0 +1,20 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace Order.Test
+{
+  [TestClass]
+  public class OrderTest
+  {
+
+    [TestMethod]
+    public void OrderContructor_ItCreatesInstanceOfOrder_Order()
+    {
+      
+    }
+    // public void Dispose()
+    // {
+      
+    // }
+  }
